adca35d949b259ba1865daabc393511e9a477505
Pablo <pablo.developer0@gmail.com>
2022-03-16 16:13:35 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(cors())
+app.options('*', cors())
+
+//middleware
+app.use(express.json())
+app.use(morgan('tiny'))
+
+//routes
+const categoriesRouter = require('./routers/categories')
+const productsRouter = require('./routers/products')
+const ordersRouter = require('./routers/orders')
+const usersRouter = require('./routers/users')
+
+const api = process.env.API_URL
+
+app.use(`${api}/categories`, categoriesRouter)
+app.use(`${api}/products`, productsRouter)
+app.use(`${api}/users`, usersRouter)
+app.use(`${api}/orders`, ordersRouter)
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected with MongoDb')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000')
+})
---
+++ backend/routers/products.js
@@ -0,0 +1,85 @@
+const { Product } = require('../models/product')
+const express = require('express')
+const { Category } = require('../models/category')
+const router = express.Router()
+
+//routes
+router.get('/', async (req, res) => {
+    const productList = await Product.find().populate('category')
+    //.select('name image -_id')
+
+    // .select('name image -_id')
+
+    if (!productList) {
+        res.status(500).json({ success: false })
+    }
+    res.send(productList)
+})
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category')
+
+    if (!product) {
+        res.status(500).json({ success: false })
+    }
+    res.send(product)
+})
+
+router.post('/', async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if (!category) {
+        return res.status(400).send('Invalid category!!')
+    }
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        // images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+    product = await product.save()
+    if (!product) {
+        return res.send(500).send('Product cannot be created')
+    }
+    res.send(product)
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if (!category) {
+        return res.status(400).send('Invalid category!!')
+    }
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            // images: req.body.images,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true }
+    )
+
+    if (!product) {
+        return res.send(500).send('Product cannot be updated! ')
+    }
+    res.send(product)
+})
+
+module.exports = router
